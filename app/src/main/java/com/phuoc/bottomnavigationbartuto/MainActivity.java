e35ebc0e46f5540109c652ba97c2cd2ba159d294
Nguyễn Đức Phước <nguyenducphuoc0802@gmail.com>
2023-09-29 12:30:34 +0700

    Initial commit

---
+++ app/src/main/java/com/phuoc/bottomnavigationbartuto/MainActivity.java
@@ -0,0 +1,32 @@
+package com.phuoc.bottomnavigationbartuto;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.action_home) {
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+    }
+}
